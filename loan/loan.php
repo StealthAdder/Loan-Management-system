7fe4eab16ae855b7c9e49867469ecb868fdebe91
StealthAdder <hariharanp2435@gmail.com>
2021-01-06 03:38:01 +0530

    page for loan details related to customer created.
    Signed-off-by: StealthAdder <hariharanp2435@gmail.com>

--- loan/loan.php
+++ loan/loan.php
@@ -1,69 +1,3 @@
-<!-- LOAN INSERTION -->
-
-<?php
-session_start();
-// $con=mysqli_connect('localhost','root','','loan management system');
-include ("/var/www/html/access/access_loan.php");
-  // //connection
-  $db = "loan_management_system";
-  $con = mysqli_connect($host, $user, $passwd, $db);
-  unset($hostname, $username, $passwd, $db);
-
-if(!$con){
-      echo'Connection error'. mysqli_connect_errno();
-}
-else {
-    echo "Connected";
-}
-
-if (isset($_POST['submit'])) {
-    // Storing values from form in vars.
-$customer_name = $_SESSION['name'];
-$customer_id = $_SESSION['customer_id'];
-
-$loan_type=$_POST["loantype"];
-$loan_amount=$_POST["loanamount"];
-$loan_tenure=$_POST["loantenure"];
-$interest_rate=$_POST["interestrate"];
-
-
-echo $customer_id;
-echo "<br>";
-echo $customer_name;
-echo "<br>";
-echo $loan_type;
-echo "<br>";
-echo $loan_amount;
-echo "<br>";
-echo $loan_tenure;
-echo "<br>";
-echo $interest_rate;
-
-// loan_id | customer_id | customer_name | loan_type | loan_amount | loan_tenure | interest_rate
-$sql = "INSERT INTO loan_details (customer_id, customer_name, loan_type, loan_amount, loan_tenure, interest_rate) VALUES (?, ?, ?, ?, ?, ?)";
-
-$stmt = mysqli_stmt_init($con);
-
-if(!mysqli_stmt_prepare($stmt, $sql)) {
-    header("Location: loan.php?msg=stmterror");
-    exit();
-    // replace with header soon.
-}
-else {
-    mysqli_stmt_bind_param($stmt, "isssss", $customer_id, $customer_name, $loan_type, $loan_amount, $loan_tenure, $interest_rate);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_store_result($stmt);
-
-    header("Location: loan.php?msg=success");
-    exit();
-}
-mysqli_stmt_close($stmt);
-mysqli_close($con);
-}
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -72,17 +6,19 @@ mysqli_close($con);
     <title>Loan</title>
 </head>
 <body>
-    
+<!-- create navigation here. -->
+  <nav>
+    <a href="/Loan-Management-system/user_home.php">Home</a>
+  </nav>
 <div class="form-popup" id="myForm">
-  <form action="/Loan-Management-system/test.php" class="form-container"  method="POST">
-  <?php echo $_SESSION['customer_id'] ?>
+  <form action="/Loan-Management-system/loan/loan.inc.create.php" class="form-container"  method="POST">
     <h1> ENTER LOAN DETAILS :</h1>
      <label for="loan type"><b>LOAN TYPE</b></label>
     <select id="loantype" name="loantype">
       <option value="#" disabled selected>Choose</option>
-      <option value="home_loan">Home Loan</option>
-      <option value="personal_loan">Personal Loan</option>
-      <option value="vehicle_loan">Vehicle Loan</option>
+      <option value="HOME LOAN">Home Loan</option>
+      <option value="PERSONAL LOAN">Personal Loan</option>
+      <option value="VEHICLE LOAN">Vehicle Loan</option>
     </select>
      <br>
      
@@ -94,9 +30,9 @@ mysqli_close($con);
      <!-- <input type="months" maxlength = "50" placeholder="Enter loan tenure" name="loantenure" required> -->
      <select id="loantenure" name="loantenure">
       <option value="#" disabled selected>Select</option>
-      <option value="24">24 Months</option>
-      <option value="60">60 Months</option>
-      <option value="84">84 Months</option>
+      <option value="24 Months">24 Months</option>
+      <option value="60 Months">60 Months</option>
+      <option value="84 Months">84 Months</option>
     </select>
      <br>
      
--- user_home.php
+++ user_home.php
@@ -116,6 +116,66 @@ body {
   }
 }
 </style>
+
+<!-- drop down css -->
+<style>
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown .dropbtn {
+  cursor: pointer;
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+/* .navbar a:hover, .dropdown:hover .dropbtn, .dropbtn:focus {
+  background-color: #333;
+} */
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+.show {
+  display: block;
+}
+</style>
+
 </head>
 <body>
 
@@ -126,7 +186,20 @@ body {
 
 <div class="navbar">
   <a href="myaccount.php">MY ACCOUNT</a>
-  <a href="./loan/loan.php">LOAN</a>
+  <!-- <a href="./loan/loan.php">LOAN</a> -->
+  
+  <div class="dropdown">
+  
+  <button class="dropbtn" onclick="myFunction()">LOAN
+    <i class="fa fa-caret-down"></i>
+  </button>
+  <div class="dropdown-content" id="myDropdown">
+    <a href="/Loan-Management-system/loan/loan.php">Apply for loan</a>
+    <a href="#">Types of loan</a>
+    <a href="/Loan-Management-system/loan/loan_details.php">View Loan Details</a>
+  </div>
+  </div> 
+  
   <a href="http:emi.php">EMI</a>
   <a href="http:loan_payment.php">LOAN PAYMENT</a>
   <!-- create session -->
@@ -267,6 +340,25 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
+
+<!-- Script for Drop down -->
+<script>
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
 }
 </script>
 
