eb70617c760f0d31604cb45e24c91f533550612f
StealthAdder <hariharanp2435@gmail.com>
2021-01-08 22:34:31 +0530

    interest model created for all types of loan
    Signed-off-by: StealthAdder <hariharanp2435@gmail.com>

--- loan/test.php
+++ loan/test.php
@@ -1,4 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test</title>
+</head>
+<body>
+    <a href="../user_home.php">HOME</a>
+</body>
+</html>
+
 <?php
+
 
 session_start();
 
@@ -64,6 +77,7 @@ if(isset($_POST['submit']))
             // if loan is more less than 10L and more than great than 5L
             if ($loan_amount >= 500000 && $loan_amount < 1000000) {
                 echo "SCHEME 1";
+                echo "<br>";
                 // check for tenuare and set the interest rate for year.
                 if ($loan_tenure === "24") {
                     $n = $loan_tenure;
@@ -95,6 +109,7 @@ if(isset($_POST['submit']))
             }
             elseif ($loan_amount >= 1000000 && $loan_amount < 5000000) {
                 echo "SCHEME 2";
+                echo "<br>";
                 if ($loan_tenure === "24") {
                     $n = $loan_tenure;
                     // interest for 24 months
@@ -125,6 +140,7 @@ if(isset($_POST['submit']))
             }
             elseif ($loan_amount >=5000000) {
                 echo "SCHEME 3";
+                echo "<br>";
                 if ($loan_tenure === "24") {
                     $n = $loan_tenure;
                     // interest for 24 months
@@ -160,6 +176,211 @@ if(isset($_POST['submit']))
         }
     }
 
+    // PERSONAL LOAN
+    
+    elseif ($loan_type === "PERSONAL LOAN") {
+
+        if ($loan_amount < "500000") {
+            // based on tenure set the interest for loan below 5L
+            echo "SCHEME 1";
+            echo "<br>";
+            if ($loan_tenure === "24") {
+                $n = $loan_tenure;
+                // interest for 24 months
+                $interest_rate = 11.55;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            elseif ($loan_tenure === "60") {                 
+                $n = $loan_tenure;
+                // interest for 60 months
+                $interest_rate = 12.50;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            elseif ($loan_tenure === "84") {
+                // interest for 84 months
+                $interest_rate = 14.8;
+                $n = $loan_tenure;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            else {
+                echo "Something wrong.";
+            }
+        }
+        elseif ($loan_amount >= "500000" && $loan_amount < "1000000") {
+            // interest based on tenure & 5L to below 10L
+            echo "SCHEME 2";
+            echo "<br>";
+            if ($loan_tenure === "24") {
+                $n = $loan_tenure;
+                // interest for 24 months
+                $interest_rate = 12.8;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            elseif ($loan_tenure === "60") {                 
+                $n = $loan_tenure;
+                // interest for 60 months
+                $interest_rate = 14.65;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            elseif ($loan_tenure === "84") {
+                // interest for 84 months
+                $interest_rate = 16.00;
+                $n = $loan_tenure;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            else {
+                echo "Something wrong.";
+            }
+        }
+        elseif ($loan_amount >= "1000000" && $loan_amount <= "1500000") {
+            echo "SCHEME 3";
+            echo "<br>";
+            if ($loan_tenure === "24") {
+                $n = $loan_tenure;
+                // interest for 24 months
+                $interest_rate = 14.60;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            elseif ($loan_tenure === "60") {                 
+                $n = $loan_tenure;
+                // interest for 60 months
+                $interest_rate = 16.00;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            elseif ($loan_tenure === "84") {
+                // interest for 84 months
+                $interest_rate = 18.52;
+                $n = $loan_tenure;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            else {
+                echo "Something wrong.";
+            }
+        }
+    }
+    
+    // VEHICLE LOAN
+
+    elseif ($loan_type === "VEHICLE LOAN") {
+        // loan amount.
+        if ($loan_amount < "150000") {
+            echo "SCHEME 1";
+            echo "<br>";
+            if ($loan_tenure === "24") {
+                $n = $loan_tenure;
+                // interest for 24 months
+                $interest_rate = 10.80;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            elseif ($loan_tenure === "60") {                 
+                $n = $loan_tenure;
+                // interest for 60 months
+                $interest_rate = 11.60;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            elseif ($loan_tenure === "84") {
+                // interest for 84 months
+                $interest_rate = 12.4;
+                $n = $loan_tenure;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            else {
+                echo "Something wrong.";
+            }
+        }
+        elseif ($loan_amount >= "150000" && $loan_amount < "800000") {
+            echo "SCHEME 2";
+            echo "<br>";
+            if ($loan_tenure === "24") {
+                $n = $loan_tenure;
+                // interest for 24 months
+                $interest_rate = 11.50;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            elseif ($loan_tenure === "60") {                 
+                $n = $loan_tenure;
+                // interest for 60 months
+                $interest_rate = 12.75;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            elseif ($loan_tenure === "84") {
+                // interest for 84 months
+                $interest_rate = 13.40;
+                $n = $loan_tenure;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            else {
+                echo "Something wrong.";
+            }
+        }
+        elseif ($loan_amount >= "800000" && $loan_amount <= "2000000") {
+            echo "SCHEME 3";
+            echo "<br>";
+            if ($loan_tenure === "24") {
+                $n = $loan_tenure;
+                // interest for 24 months
+                $interest_rate = 12.80;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            elseif ($loan_tenure === "60") {                 
+                $n = $loan_tenure;
+                // interest for 60 months
+                $interest_rate = 14.05;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            elseif ($loan_tenure === "84") {
+                // interest for 84 months
+                $interest_rate = 15.80;
+                $n = $loan_tenure;
+                echo "I = ".$interest_rate;
+                echo "<br>";
+                EMI($loan_amount, $interest_rate, $n);
+            }
+            else {
+                echo "Something wrong.";
+            }
+        }
+
+    }
+    else {
+        echo "Not eligiable for any loan or there is some error.";
+    }
+    
+    
     // SQL
 
 }
